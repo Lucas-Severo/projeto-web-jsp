40ec66a14f8dde7342c4aef7b58748db1586950e
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-05-13 09:54:17 -0400

    Mostrando o ciclo de vida do Servlet

--- projetoweb/src/main/java/projetoweb/ClienteServlet.java
+++ projetoweb/src/main/java/projetoweb/ClienteServlet.java
@@ -11,10 +11,32 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = { "/cliente", "/clienteServlet", "/clienteController" })
 public class ClienteServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7371218363017857325L;
+
+	public ClienteServlet() {
+		System.out.println("Construindo o servlet...");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Inicializando o Servlet");
+		super.init();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Chamando o Servlet");
+		super.service(req, resp);
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("Chamou pelo método get");
+		System.out.println("Chamou pelo método get");
 	}
 
 	@Override
@@ -33,6 +55,12 @@ public class ClienteServlet extends HttpServlet {
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("Chamou pelo método PUT");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("Destruindo Servlet...");
+		super.destroy();
 	}
 	
 }
